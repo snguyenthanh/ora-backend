3cfe01bcc98ddf16a687e1cc1cde6a3d921f8640
Son <son@fr0st.local>
2019-11-09 02:03:34 +0800

    Try out setting origin for cors

--- ora_backend/exceptions.py
+++ ora_backend/exceptions.py
@@ -4,7 +4,7 @@ A collection of custom exceptions to return to client.
 from sanic.response import json
 from sanic.exceptions import SanicException
 
-from ora_backend.config import CORS_ORIGINS
+# from ora_backend.config import CORS_ORIGINS
 
 
 def sanic_error_handler(request, exception):
@@ -19,11 +19,16 @@ def sanic_error_handler(request, exception):
     if isinstance(exc_message, str):
         exc_message = {"msg": [exc_message]}
 
+    if "ORIGIN" in request.headers:
+        access_control_allow_origin = request.headers["ORIGIN"]
+    else:
+        access_control_allow_origin = "https://chatwithora.com"
+
     return json(
         {"error": exc_message},
         status=status_code,
         headers={
-            "Access-Control-Allow-Origin": "https://chatwithora.com",
+            "Access-Control-Allow-Origin": access_control_allow_origin,
             "Access-Control-Allow-Credentials": "true",
         },
     )
