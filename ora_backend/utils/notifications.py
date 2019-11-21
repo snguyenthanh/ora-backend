ab8ba66e9a6e15dded8a29d8bd597aefc46e6f8d
Son <son@fr0st.local>
2019-11-21 21:17:43 +0800

    Fix send noti bug

--- ora_backend/utils/notifications.py
+++ ora_backend/utils/notifications.py
@@ -3,8 +3,8 @@ from ora_backend.models import User, NotificationStaff
 
 
 async def send_notifications_to_all_high_ups(content: dict):
-    all_high_ups_users = await User.query.get(User.role_id < ROLES.inverse["agent"])
+    all_high_ups_users = await User.query.where(User.role_id < ROLES.inverse["agent"]).gino.all()
     notifications = [
-        {"staff_id": user["id"], "content": content} for user in all_high_ups_users
+        {"staff_id": user.id, "content": content} for user in all_high_ups_users
     ]
     await NotificationStaff.bulk_upsert(notifications)
